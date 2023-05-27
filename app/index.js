e43e7f4952b8a6b0a6ee7f41e633fc44a9f8d110
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-05-27 14:10:34 +0600

    main: add new buttons with styles

--- app/index.js
+++ app/index.js
@@ -1,13 +1,4 @@
-import surah105 from '../public/105.png';
-import surah106 from '../public/106.png';
-import surah107 from '../public/107.png';
-import surah108 from '../public/108.png';
-import surah109 from '../public/109.png';
-import surah110 from '../public/110.png';
-import surah111 from '../public/111.png';
-import surah112 from '../public/112.png';
-import surah113 from '../public/113.png';
-import surah114 from '../public/114.png';
+
 import React from "react";
 import {
   ScrollView,
@@ -20,16 +11,11 @@ import {
 
 const App = () => {
     const cards = [
-      { image: surah105, text: 'Al-Fil' },
-      { image: surah106, text: 'Quraysh' },
-      { image: surah107, text: 'Al-Ma\'un' },
-      { image: surah108, text: 'Al-Kawthar' },
-      { image: surah109, text: 'Al-Kafirun' },
-      { image: surah110, text: 'An-Nasr' },
-      { image: surah111, text: 'Lahab' },
-      { image: surah112, text: 'Al-Ikhlas' },
-      { image: surah113, text: 'Al-Falaq' },
-      { image: surah114, text: 'An-Nas' },
+      { text: 'নবীদের নিয়ে আয়াত সমূহ' },
+      { text: 'আয়াতুল কুরসি' },
+      {  text: 'সুরা ফাতিহা' },
+      { text: 'আয়াতুল কুরসি' },
+      {  text: '৫ টি দোয়া' },
     ];
 
   return (
@@ -38,7 +24,6 @@ const App = () => {
       <ScrollView contentContainerStyle={styles.scrollContainer}>
       {cards.map((item, index) => (
           <TouchableOpacity key={index} style={styles.button}>
-            <Image source={item.image} style={styles.buttonImage} />
             <Text style={styles.buttonText}>{item.text}</Text>
           </TouchableOpacity>
         ))}
@@ -50,6 +35,7 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
   },
   scrollContainer: {
     alignItems: "center",
@@ -61,10 +47,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 10,
+    borderRadius: "0.25rem",
+    width:500,
   },
   buttonText: {
     color: "white",
     fontSize: 16,
+    display: "flex",
+    alignContent: "center",
+    textAlign: "center",
   },
   buttonImage: {
     width: '100%',
