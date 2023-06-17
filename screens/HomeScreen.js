bd2be395264e88e83a7d3d021d85c0a6e30f7f63
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-06-17 22:27:55 +0600

    add cards in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import React, { useLayoutEffect } from 'react';
+import { Text, View } from 'react-native';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -8,12 +8,39 @@ const HomeScreen = () => {
         navigation.setOptions({
             headerShown: false,
         }, [])
-    })
-  return (
-    <View>
-      <Text className="text-red-800 font-semibold text-center">HomeScreen</Text>
-    </View>
-  )
-}
+    });
 
+    const cardItems = [
+      { name: 'Surah Asor', image: require('../public/first.png') },
+      { name: 'Surah Fatiha', image: require('../public/third.png') },
+      { name: 'Ayatul Kursi', image: require('../public/second.png') },
+      { name: 'Surah Ikhlas', image: require('../public/first.png') },
+      { name: 'Surah Bakarah', image: require('../public/second.png') },
+      { name: 'Surah Nas', image: require('../public/third.png') },
+    ];
+
+
+return (
+  <View className="flex-1 p-4">
+    <View className="flex-row mb-4">
+      <CardItem item={cardItems[0]} />
+      <CardItem item={cardItems[1]} />
+    </View>
+    <View className="flex-row mb-4">
+      <CardItem item={cardItems[2]} />
+      <CardItem item={cardItems[3]} />
+    </View>
+    <View className="flex-row mb-4">
+      <CardItem item={cardItems[4]} />
+      <CardItem item={cardItems[5]} />
+    </View>
+  </View>  );
+};
+
+const CardItem = ({ item }) => (
+  <View className="w-1/2 p-4 bg-green-800 border-1">
+      {/* <Image src={item.image} className="w-5 h-5 object-cover rounded-lg" /> */}
+    <Text className="text-xl font-bold mt-2">{item.name}</Text>
+  </View>
+);
 export default HomeScreen;
