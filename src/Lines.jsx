bafd822243fa4b51537506a42f63839980157b8b
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-01-25 22:25:00 +0600

    add sepia theme in sepia theme branch

--- src/Lines.jsx
+++ src/Lines.jsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 import HoverCard from "./HoverCard";
 import PropTypes from 'prop-types';
 
-// word, meaning, word-type, list of similar word of word-type, hover-all
-const Lines =({ayah, number})=> {
- 
+const Lines = ({ ayah, number }) => {
   const [hoveredWord, setHoveredWord] = useState(null);
 
   const handleWordHover = (wordData) => {
@@ -15,40 +13,30 @@ const Lines =({ayah, number})=> {
     }
   };
 
-
-
-  
   return (
     <div className="relative flex flex-row mt-16 ml-24">
-    <h1 className="font-bold text-2xl text-teal-700">{number}</h1>
-  
- {ayah.slice().reverse().map((item, index) => (
-        <div key={index}>
-          <p  
-           className={item.card ? 'font-bold text-2xl text-red-700' : 'text-2xl text-sky-900'}
+      <h1 className="font-bold text-2xl text-teal-700">{number}</h1>
 
+      {ayah.slice().reverse().map((item, index) => (
+        <div key={index}>
+          <p
+            className={item.card ? 'font-bold text-2xl text-red-700 filter sepia' : 'text-2xl text-sky-900 filter sepia'}
             onMouseOver={() => handleWordHover(item)}
             onMouseOut={() => handleWordHover(null)}
           >
             {item.word}
           </p>
-          {/* <ul>
-            {item.definition.map((definitionItem, definitionIndex) => (
-              <li key={definitionIndex}>{definitionItem}</li>
-            ))}
-          </ul> */}
         </div>
-      ))}   
-         {hoveredWord && <HoverCard word={hoveredWord.word} definition={hoveredWord.definition} />}
+      ))}
 
-    
+      {hoveredWord && <HoverCard word={hoveredWord.word} definition={hoveredWord.definition} />}
     </div>
-  )
-}
+  );
+};
 
 Lines.propTypes = {
   ayah: PropTypes.object.isRequired,
   number: PropTypes.string.isRequired,
 };
 
-export default Lines
+export default Lines;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      filter: {
+        sepia: 'sepia(100%)',
+      },
+    },
   },
   plugins: [],
 }
